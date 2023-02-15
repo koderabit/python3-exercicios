3ce8794ab8ef8b9afd6352fef67b8cf43f4957f4
KoderaBit <123130797+koderabit@users.noreply.github.com>
2023-02-15 16:09:17 -0300

    Update ex032.py

--- ex032.py
+++ ex032.py
@@ -1,8 +1,7 @@
 #Faça um programa que pergunte um ano qualquer e mostre na tela se ele é um ano bissexto.
 ano = int(input("Se você tem dúvidas se um determinado ano é bissexto, basta informá-lo e eu responderei: "))
-if(ano%4==0 and ano%100!=0):
+if():
     print("É um ano Bissexto.")
-elif(ano%4!=0 and ano%400==0):
-    print(("É um ano Bissexto."))
 else:
-    print("Não é um ano Bissexto.")
+    print(("Não é um ano Bissexto."))
+#falta incluir a condição;
